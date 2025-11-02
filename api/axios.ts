e2c6e6e869f9f266e94f7861e70d8099b21a28bf
felliks-c <feliksnekot@gmail.com>
2025-11-03 00:29:05 +0500

    setted and epdated api uses

--- api/axios.ts
+++ api/axios.ts
@@ -1,3 +1,4 @@
+// api/axios.ts
 import axios from "axios";
 
 const instance = axios.create({
--- app/api/vendors/route.ts
+++ app/api/vendors/route.ts
@@ -1,7 +1,9 @@
-// src/app/api/vendors/route.ts
+// app/api/vendors/route.ts
 import { NextRequest, NextResponse } from 'next/server';
 import { useCreateVendor } from '@/composables/server/useCreateVendor';
 import { useVendors } from '@/composables/server/useVendors';
+
+
 
 export async function GET(req: NextRequest) {
   const searchParams = Object.fromEntries(new URL(req.url).searchParams.entries());
