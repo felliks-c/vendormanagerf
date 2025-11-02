dd138473cf88130e3df50015574a849932a07e37
felliks-c <feliksnekot@gmail.com>
2025-11-03 04:21:24 +0500

    fixed client composable

--- composables/client/useDeleteVendor.ts
+++ composables/client/useDeleteVendor.ts
@@ -1,22 +1,3 @@
-// // composables/client/useDeleteVendor.ts
-// import axios from '@/api/axios';
-// import { VendorDeleteInput } from '@/composables/server/types';
-
-// export const useDeleteVendor = async (data: VendorDeleteInput): Promise<string | null> => {
-//   try {
-//     const res = await axios.delete<{ message: string }>('/api/vendors', { data });
-//     return res.data.message;
-//   } catch (err: any) {
-//     console.error('Error deleting vendor:', err.response?.data || err.message);
-//     return null;
-//   }
-// };
-
-
-
-
-
-
 // composables/client/useDeleteVendor.ts
 import axios from '@/api/axios';
 import { VendorDeleteInput } from '@/composables/server/types';
