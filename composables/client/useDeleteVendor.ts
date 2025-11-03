8cfacf953f621911570651ce261bdd6587c75c00
felliks-c <feliksnekot@gmail.com>
2025-11-03 05:33:24 +0500

    fixed composables & using routes

--- composables/client/useDeleteVendor.ts
+++ composables/client/useDeleteVendor.ts
@@ -5,7 +5,7 @@ import { VendorDeleteInput } from '@/composables/server/types';
 export const useDeleteVendor = async (data: VendorDeleteInput): Promise<string | null> => {
   try {
     // Клиент → Next.js API → FastAPI
-    const res = await axios.delete<{ message: string }>(`/api/vendors/${data.id}`);
+    const res = await axios.delete<{ message: string }>(`/api/vendor/${data.id}`);
     return res.data.message;
   } catch (err: any) {
     console.error('Error deleting vendor:', err.response?.data || err.message);
